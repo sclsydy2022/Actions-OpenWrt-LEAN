6190ab64f24eb834c4d7e89df1ed0e6f0137c7d7
sclsydy2022 <98293456+sclsydy2022@users.noreply.github.com>
2025-04-14 13:13:09 +0800

    Update DW33D-part1.sh

--- DW33D-part1.sh
+++ DW33D-part1.sh
@@ -14,9 +14,10 @@
 sed -i '/;openwrt-23.05/d' feeds.conf.default
 sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -26,15 +27,9 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-# 更新包源
-# ./scripts/feeds update -a
-
 # 解决冲突
 # rm -rf feeds/luci/applications/luci-app-mosdns
 # rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 # rm -rf feeds/packages/utils/v2dat
 # rm -rf feeds/packages/lang/golang
 # git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-
-# 安装包
-# ./scripts/feeds install -a 
