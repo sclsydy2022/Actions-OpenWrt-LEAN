492ca022b4fe3cd39a44ce3557aab88ab308a586
sclsydy2022 <98293456+sclsydy2022@users.noreply.github.com>
2025-04-16 22:55:42 +0800

    Update DW33D-part2.sh

--- DW33D-part2.sh
+++ DW33D-part2.sh
@@ -32,7 +32,7 @@ git clone https://github.com/leshanydy2022/adguardhome.git feeds/packages/net/ad
 
 # 为smartDNS插件更换最新的版本
 rm -rf feeds/packages/net/smartdns
-git clone https://github.com/sclsydy2022/smartdns feeds/packages/net/smartdns
+git clone https://github.com/sclsydy2022/smartdns.git feeds/packages/net/smartdns
 
 # Modify hostname
 sed -i 's/LEDE/LEAN-YDY/g' package/base-files/files/bin/config_generate
