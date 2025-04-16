df3a1b04141abaaa0bf800f2452c5ae99b7deaf2
sclsydy2022 <98293456+sclsydy2022@users.noreply.github.com>
2025-04-16 22:10:27 +0800

    Update DW33D-part1.sh

--- DW33D-part1.sh
+++ DW33D-part1.sh
@@ -13,7 +13,7 @@
 # Uncomment a feed source
 sed -i '/;openwrt-23.05/d' feeds.conf.default
 sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
