9c632d6878a47e44c98246e72b03f60ffc405591
sclsydy2022 <98293456+sclsydy2022@users.noreply.github.com>
2025-05-02 13:59:24 +0800

    Update DW33D-part2.sh

--- DW33D-part2.sh
+++ DW33D-part2.sh
@@ -23,7 +23,8 @@ sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap-mod/g' feeds/luci/collection
 # 安装luci-app-smartdns和luci-app-adguardhome
 rm -rf feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/luci/applications/luci-app-adguardhome
-git clone https://github.com/sclsydy2022/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+# git clone https://github.com/sclsydy2022/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+git clone https://github.com/leshanydy2022/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/leshanydy2022/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 
 # 为adguardhome插件更换最新的版本
@@ -32,7 +33,8 @@ git clone https://github.com/leshanydy2022/adguardhome.git feeds/packages/net/ad
 
 # 为smartDNS插件更换最新的版本
 rm -rf feeds/packages/net/smartdns
-git clone https://github.com/sclsydy2022/smartdns.git feeds/packages/net/smartdns
+# git clone https://github.com/sclsydy2022/smartdns.git feeds/packages/net/smartdns
+git clone https://github.com/leshanydy2022/smartdns.git feeds/packages/net/smartdns
 
 # Modify hostname
 sed -i 's/LEDE/LEAN-YDY/g' package/base-files/files/bin/config_generate
